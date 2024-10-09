b858a2413679068a55cc7f55930d4a9218931a6c
Juliana Guimarães de Farias <41305417+jugfarias@users.noreply.github.com>
2024-10-09 18:54:24 -0300

    Delete Aula20-23_ModBonus/ReactReminder directory

--- Aula20-23_ModBonus/ReactReminder/src/components/Hero/index.tsx
+++
@@ -1,8 +0,0 @@
-import { Container } from "./styles";
-import heroImg from "../../assets/images/hero.png";
-
-export function Hero() {
-    return <Container>
-        <img src={heroImg} alt="Paisagem em tons roxos" />
-    </Container>;
-}
--- Aula20-23_ModBonus/ReactReminder/src/components/Logo/index.tsx
+++
@@ -1,6 +0,0 @@
-import { LogoImage } from "./styles";
-import RRLogo from "../../assets/images/RRLogo.jpg";
-
-export function Logo() {
-    return <LogoImage src={RRLogo} alt="Logo React Reminder" />
-}
